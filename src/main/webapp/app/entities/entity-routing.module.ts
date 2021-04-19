72ec427c2213c581c6314f1e65604c127f7e0b61
Hossein Jafari <hosijafari@gmail.com>
2021-04-19 16:10:14 -0400

    add professor and course

--- src/main/webapp/app/entities/entity-routing.module.ts
+++ src/main/webapp/app/entities/entity-routing.module.ts
@@ -4,6 +4,16 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   imports: [
     RouterModule.forChild([
+      {
+        path: 'courses',
+        data: { pageTitle: 'Courses' },
+        loadChildren: () => import('./courses/courses.module').then(m => m.CoursesModule),
+      },
+      {
+        path: 'professor',
+        data: { pageTitle: 'Professors' },
+        loadChildren: () => import('./professor/professor.module').then(m => m.ProfessorModule),
+      },
       /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
     ]),
   ],
